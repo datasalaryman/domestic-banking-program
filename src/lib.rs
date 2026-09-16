25affc437301f4b93d70d321b8310ce6afea0845
Data Salaryman <datasalaryman@protonmail.com>
2026-09-17 07:28:54 +0800

    chore: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,23 @@
+#![cfg_attr(not(test), no_std)]
+
+use quasar_lang::prelude::*;
+
+mod errors;
+mod instructions;
+mod state;
+use instructions::*;
+
+declare_id!("9ZyWG6ZceKcHy9fXRGLDJqZPHNPJtcmQqHAkKVMxidhW");
+
+#[program]
+mod domestic_banking_program {
+    use super::*;
+
+    #[instruction]
+    pub fn initialize(ctx: Ctx<Initialize>) -> Result<(), ProgramError> {
+        ctx.accounts.initialize()
+    }
+}
+
+#[cfg(test)]
+mod tests;
